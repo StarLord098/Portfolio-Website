a529027cb6b9d7748657ac30911b37b996e9b2c3
Keshav Sharma <100477734+StarLord098@users.noreply.github.com>
2024-09-04 02:40:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { About } from './components/about';
+import { About } from './components/About';
 import { Technologies } from './components/Technologies';
 import { Projects } from './components/Projects';
 import { Contact } from './components/contact';
